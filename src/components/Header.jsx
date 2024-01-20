20b926d66c88ad057750f4d209afbbea8f337b62
MickMacha <kimmike1100@gmail.com>
2024-01-20 22:20:51 +0300

    added searchbar and help icon in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 
 import Avatar from '@mui/material/Avatar';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
 
 const Header = () => (
     <div className='header'>
@@ -15,7 +18,19 @@ const Header = () => (
         </HeaderLeft>
         
         {/* Header Search */}
+
+        <HeaderSearch>
+            {/* Search Icon */}
+            <SearchIcon />
+            
+            {/* Input */}
+            <input placeholder='Search Slack' />
+        </HeaderSearch>
         {/* Header Right */}
+        
+        <HeaderRight>
+        <HelpOutlineIcon />
+        </HeaderRight >
         </HeaderCont>
         
     </div>
@@ -51,3 +66,35 @@ cursor: pointer;
 :hover {
     opacity: 0.8;
 }`;
+
+const HeaderSearch = styled.div`
+
+flex: 0.4;
+opacity: 1;
+border-radius: 6px;
+background-color: #421f44;
+text-align: center;
+color:gray;
+padding: 0 50px;
+border: 1px solid gray;
+
+> input{
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+}
+`;
+
+const HeaderRight = styled.div`
+flex: 0.3;
+display: flex;
+align-items: flex-end;
+
+> .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+}
+`;
